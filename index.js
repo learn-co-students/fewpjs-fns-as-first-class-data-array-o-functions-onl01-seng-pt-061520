3380ef90041400e3cf0fe9485b2b64d3b31cd2d2
Igor Veyner <igorveyner95@gmail.com>
2020-12-08 13:34:30 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function wakeDog(name, breed) {
+  return `Wake ${name} the ${breed}`
+}
+
+function leashDog(name, breed) {
+  return `Leash ${name} the ${breed}`
+}
+
+function walkToPark(name, breed) {
+  return `Walk to the park with ${name} the ${breed}`
+}
+
+function throwFrisbee(name, breed) {
+  return `Throw the frisbee for ${name} the ${breed}`
+}
+
+function walkHome(name, breed) {
+  return `Walk home with ${name} the ${breed}`
+}
+
+function unleashDog(name, breed) {
+  return `Unleash ${name} the ${breed}`
+}
+
+var routine = [
+  wakeDog,
+  leashDog,
+  walkToPark,
+  throwFrisbee,
+  walkHome,
+  unleashDog
+]
+
+function exerciseDog(name, breed) {
+  return routine.map(fn => fn(name, breed))
+}
